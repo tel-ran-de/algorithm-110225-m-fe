82bfb86748ec27c9dbc26efd19c52fe0e5bbe344
dimarique <pochtagreen@icloud.com>
2025-04-03 22:02:22 +0200

    Update hw02_sortReverse.js

--- src/main/java/org/telran/lecture_02_big_o/home_work/hw02_sortReverse.js
+++ src/main/java/org/telran/lecture_02_big_o/home_work/hw02_sortReverse.js
@@ -2,7 +2,18 @@
 // Измените алгоритм сортировки так, чтобы сортировка шла в обратном порядке (от большего к меньшему)
 
 // Оцените сложность алгоритма
+//   O(n²)
 
 function reverseBubbleSort(arr) {
-
+	let n = arr.length;
+	for (let i = 0; i < n - 1; i++) {
+		for (let j = 0; j < n - i - 1; j++) {
+			if (arr[j] < arr[j + 1]) {
+				let temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	return arr;
 }
