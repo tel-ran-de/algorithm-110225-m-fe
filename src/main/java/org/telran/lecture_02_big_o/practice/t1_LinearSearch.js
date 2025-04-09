2e977a1ec248a683807e0fe440d91d0d617343f9
Kanyshai Sharapova <80043552+Kany-08@users.noreply.github.com>
2025-04-09 23:47:08 +0200

    Update t1_LinearSearch.js

--- src/main/java/org/telran/lecture_02_big_o/practice/t1_LinearSearch.js
+++ src/main/java/org/telran/lecture_02_big_o/practice/t1_LinearSearch.js
@@ -7,6 +7,11 @@
 
 // Решение
 function linear_search(numbers, target_number) {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] === target_number) {
+            return i;
+        }
+    }
     return -1;
 }
 console.log(linear_search([2, 3, 5], 5));
