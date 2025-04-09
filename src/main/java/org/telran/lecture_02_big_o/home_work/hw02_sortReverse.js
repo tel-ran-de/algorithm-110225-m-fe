ba27ba4c417389a9320f56322f9797eaaa8c6e44
JanaWalt <jana.plotkina@googlemail.com>
2025-04-09 15:45:36 +0200

    Update hw02_sortReverse.js

--- src/main/java/org/telran/lecture_02_big_o/home_work/hw02_sortReverse.js
+++ src/main/java/org/telran/lecture_02_big_o/home_work/hw02_sortReverse.js
@@ -4,5 +4,25 @@
 // Оцените сложность алгоритма
 
 function reverseBubbleSort(arr) {
-
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+              
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
 }
+
+let arr = [64, 34, 25, 12, 22, 11, 90];
+console.log("Отсортированный массив:", reverseBubbleSort(arr));
+
+
+// Оцените сложность алгоритма
+
+
+O(n²)
